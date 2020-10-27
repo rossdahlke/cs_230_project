b7cc4d1b5e894c7a40649613564bdebcc124d9c4
rossdahlke <ross.dahlke@gmail.com>
2020-10-26 18:08:54 -0700

    calculated similarities on first and last 1/10th of transcripts

--- 03_milestone_nlp.py
+++ 03_milestone_nlp.py
@@ -12,37 +12,62 @@
 # There will be three models that I will train
 # 1. tf-idf with some sort of regression
 # 2. RNN with pre-trained sentiment
-# 3. Baseline transformers
+# 3. LSTM (on text)
+# 4. Various transformers
 
 ### tf-idf
 
 ## going to use spacy's vector similarity functionality
 import spacy
+nlp = spacy.load("en_core_web_lg")
 import docx
 import pandas as pd
 import numpy as np
 
-deltas = pandas.read_csv("data/processed/survey/opinion_deltas.csv")
+deltas = pd.read_csv("data/processed/survey/opinion_deltas.csv")
 similarity_list = []
-delta_values = []
+delta_list = []
+id = deltas["id"][0]
 for id in deltas["id"]:
-    print(id)
-    similarities = []
+    n_breaks = 10
     try:
         doc = docx.Document("data/processed/transcripts/" + id + ".docx")
         paragraphs = [p.text for p in doc.paragraphs][:-10]
-        for i in range(len(paragraphs)-1):
-            similarity = nlp(paragraphs[i]).similarity(nlp(paragraphs[i+1]))
-            similarities.append(similarity)
-        similarity_list.append(similarities)
-        delta_values.append(deltas[deltas["id"] == id]["delta"].values[0])
+        break_index = round(len(paragraphs) / n_breaks)
+        s = " "
+        text_0 = nlp(s.join(paragraphs[:break_index]))
+        text_1 = nlp(s.join(paragraphs[-break_index * (n_breaks - 1):]))
+        similarity = text_0.similarity(text_1)
+        similarity_list.append(similarity)
+        delta = abs(deltas[deltas["id"] == id]["delta"].values[0])
+        delta_list.append(delta)
+        print()
+        print(id)
+        print(similarity)
+        print(delta)
     except Exception:
         print("passed on " + id + ". No transcript.")
         pass
 
 
 
-## sentiment
+## baseline NN
+# using this tutorial https://www.kaggle.com/purplejester/a-simple-lstm-based-time-series-classifier
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+similarity_tensors = []
+for i in range(len(similarity_list)):
+    similarity_tensors.append(torch.tensor(similarity_list[i]))
 
-## transformers
+similarity_tensors_padded = pad_sequence(similarity_tensors)
