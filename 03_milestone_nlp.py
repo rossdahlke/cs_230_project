cad47ba45e66a8a9c3bc26c0289ebcd2e507fea8
rossdahlke <ross.dahlke@gmail.com>
2020-10-30 08:39:07 -0700

    NN for vector embeddings

--- 03_milestone_nlp.py
+++ 03_milestone_nlp.py
@@ -70,8 +70,6 @@ for id in deltas["id"]:
 
 deltas_similarities_dropped = deltas_similarities[['delta', '0_1', '0_2', '0_3', '0_4', '0_5', '0_6', '0_7', '0_8', '0_9', '1_2', '1_3', '1_4', '1_5', '1_6', '1_7', '1_8', '1_9', '2_3', '2_4', '2_5', '2_6', '2_7', '2_8', '2_9', '3_4', '3_5', '3_6', '3_7', '3_8', '3_9', '4_5', '4_6', '4_7', '4_8', '4_9', '5_6', '5_7', '5_8', '5_9', '6_7', '6_8', '6_9', '7_8', '7_9', '8_9']]
 
-sim_matrix_ols = ols('delta ~ Q("0_1") + Q("0_2") + Q("0_3") + Q("0_4") + Q("0_5") + Q("0_6") + Q("0_7") + Q("0_8") + Q("0_9") + Q("1_2") + Q("1_3") + Q("1_4") + Q("1_5") + Q("1_6") + Q("1_7") + Q("1_8") + Q("1_9") + Q("2_3") + Q("2_4") + Q("2_5") + Q("2_6") + Q("2_7") + Q("2_8") + Q("2_9") + Q("3_4") + Q("3_5") + Q("3_6") + Q("3_7") + Q("3_8") + Q("3_9") + Q("4_5") + Q("4_6") + Q("4_7") + Q("4_8") + Q("4_9") + Q("5_6") + Q("5_7") + Q("5_8") + Q("5_9") + Q("6_7") + Q("6_8") + Q("6_9") + Q("7_8") + Q("7_9") + Q("8_9")', deltas_similarities_dropped)
-
 ## linear regression
 trn_idx, test_idx = train_test_split(np.arange(101), test_size = .1, random_state = 1)
 
@@ -100,4 +98,88 @@ mean_squared_error(deltas_similarities_dropped[["delta"]].iloc[test_idx], test_p
 
 r2_score(deltas_similarities_dropped[["delta"]].iloc[test_idx], test_pred)
 
-## going to try to fit a DL model
+## going to try to fit a basic NN
+# https://www.kaggle.com/aakashns/pytorch-basics-linear-regression-from-scratch
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+import torch.nn.functional as F
+
+targets = np.reshape(np.array(deltas_similarities_dropped["delta"]), (-1, 1))
+inputs = deltas_similarities_dropped.drop(columns = "delta").values
+
+# The rest of data will be test set
+data_length = len(targets)
+idx_array = np.array(list(range(0, data_length)))
+np.random.shuffle(idx_array)
+train_idx = idx_array[:int(data_length * .7)]
+val_idx = idx_array[int(data_length * .7):int(data_length * .9)]
+test_idx = idx_array[int(data_length * .9):]
+
+train_inputs = torch.tensor(inputs[train_idx])
+train_targets = torch.tensor(targets[train_idx])
+val_inputs = torch.tensor(inputs[val_idx])
+val_targets = torch.tensor(targets[val_idx])
+test_inputs = torch.tensor(inputs[test_idx])
+test_targets = torch.tensor(targets[test_idx])
+
+train_ds = TensorDataset(train_inputs.float(), train_targets.float())
+val_ds = TensorDataset(val_inputs.float(), val_targets.float())
+test_ds = TensorDataset(test_inputs.float(), test_targets.float())
+batch_size = 5
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+val_dl = DataLoader(val_ds, batch_size, shuffle=True)
+test_dl = DataLoader(test_ds, batch_size, shuffle=True)
+
+class SimpleNet(nn.Module):
+    # Initialize the layers
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(45, 20)
+        self.act1 = nn.ReLU() # Activation function
+        self.linear2 = nn.Linear(20, 1)
+
+    # Perform the computation
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.act1(x)
+        x = self.linear2(x)
+        return x
+
+model = SimpleNet()
+
+opt = torch.optim.SGD(model.parameters(), lr=1e-5)
+criterion = F.mse_loss
+
+def fit(num_epochs, model, loss_fn, opt, print_every):
+    counter = 0
+    for epoch in range(num_epochs):
+        counter += 1
+        for xb, yb in train_dl:
+            # Generate predictions
+            pred = model(xb.float())
+            loss = criterion(pred.float(), yb.float())
+            # Perform gradient descent
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+
+        # loss stats
+        if counter % print_every == 0:
+            # Get validation loss
+
+            val_losses_in_itr = []
+
+            for xb, yb in val_dl:
+                # Generate predictions
+                pred = model(xb.float())
+                val_loss = criterion(pred.float(), yb.float())
+                val_losses_in_itr.append(val_loss.item())
+
+            print("Epoch: {:2d}/{:2d}\t".format(epoch+1, num_epochs),
+                  "Train Loss: {:.6f}\t".format(loss.item()),
+                  "Val Loss: {:.6f}\t".format(np.mean(val_losses_in_itr)))
+
+fit(3000, model, criterion, opt, print_every = 100)
+
+criterion(model(test_inputs.float()), test_targets.float())
