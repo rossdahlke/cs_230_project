5fd2d17baeb2a5843db048177b861d68de89dd5a
rossdahlke <ross.dahlke@gmail.com>
2020-10-30 17:22:31 -0700

    milestone experiment/ results/ discussion

--- 03_milestone_nlp.py
+++ 03_milestone_nlp.py
@@ -74,6 +74,8 @@ deltas_similarities_dropped = deltas_similarities[['delta', '0_1', '0_2', '0_3',
 ## linear regression
 trn_idx, test_idx = train_test_split(np.arange(101), test_size = .1, random_state = 1)
 
+model = LinearRegression()
+
 # Just 1_9
 
 X = deltas_similarities_dropped[['1_9']]
@@ -86,7 +88,6 @@ mean_squared_error(deltas_similarities_dropped[["delta"]].iloc[test_idx], test_p
 
 r2_score(deltas_similarities_dropped[["delta"]].iloc[test_idx], test_pred)
 
-model = LinearRegression()
 
 # all similarities
 X = deltas_similarities_dropped[['0_1', '0_2', '0_3', '0_4', '0_5', '0_6', '0_7', '0_8', '0_9', '1_2', '1_3', '1_4', '1_5', '1_6', '1_7', '1_8', '1_9', '2_3', '2_4', '2_5', '2_6', '2_7', '2_8', '2_9', '3_4', '3_5', '3_6', '3_7', '3_8', '3_9', '4_5', '4_6', '4_7', '4_8', '4_9', '5_6', '5_7', '5_8', '5_9', '6_7', '6_8', '6_9', '7_8', '7_9', '8_9']]
