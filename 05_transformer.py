0ba76ca3ea693d857282932eefd504225b679b49
rossdahlke <ross.dahlke@gmail.com>
2020-11-15 11:01:54 -0800

    batch size

--- 05_transformer.py
+++ 05_transformer.py
@@ -85,8 +85,8 @@ def dummy_data_collector(features):
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
     num_train_epochs=10,              # total # of training epochs
-    per_device_train_batch_size=20,  # batch size per device during training
-    per_device_eval_batch_size=100,   # batch size for evaluation
+    per_device_train_batch_size=10,  # batch size per device during training
+    per_device_eval_batch_size=10,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir='./logs'
