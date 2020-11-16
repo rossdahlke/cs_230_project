35aee4810a7c26431b17c317d680c378fd725d99
rossdahlke <ross.dahlke@gmail.com>
2020-11-15 20:08:35 -0800

    one more bert

--- 05_transformer_bert.py
+++ 05_transformer_bert.py
@@ -96,13 +96,13 @@ def dummy_data_collector(features):
 
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
-    num_train_epochs=100,              # total # of training epochs
+    num_train_epochs=200,              # total # of training epochs
     per_device_train_batch_size=1,  # batch size per device during training
     per_device_eval_batch_size=1,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir='./logs',
-    save_total_limit=1,
+    save_total_limit=4,
 )
 
 trainer = Trainer(
